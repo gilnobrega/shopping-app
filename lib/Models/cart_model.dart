573230640793d4a0460e64cf73af15787a24389e
Gil Nobrega <jgilfnuk@gmail.com>
2022-05-23 00:56:25 +0100

    Adds ability to get offers for a specific item

--- lib/Models/cart_model.dart
+++ lib/Models/cart_model.dart
@@ -1,14 +1,21 @@
 import 'package:shopping_app/Core/Offers/offer.dart';
 import 'package:shopping_app/Core/currency.dart';
 import 'package:shopping_app/Core/price.dart';
+import 'package:shopping_app/Models/item_model.dart';
 
 class CartModel {
   final List<Offer> availableOffers;
+  final List<ItemModel> availableItems;
 
   //dictionary with item id and number of item id that are added to cart
   final Map<int, int> items = {};
 
   final Currency currency;
+
+  CartModel(
+      {required this.currency,
+      required this.availableOffers,
+      required this.availableItems});
 
   //increments number of items with this itemid
   void addItem(int itemId) {
@@ -25,12 +32,17 @@ class CartModel {
     items[itemId] = items[itemId]! - 1;
   }
 
-  Price getPriceForItem(int itemId) {
-    int itemCount = items[itemId] ?? 0;
+  //Gets available offers for a specific item
+  Iterable<Offer> getOffersForItem({required int itemId}) {
+    return availableOffers.where((offer) => itemId == offer.itemId);
+  }
+
+  Price getPriceForItem({required int itemId, int? itemCount}) {
+    //gets item count from list of items if item count is not specified
+    itemCount = itemCount ?? items[itemId] ?? 0;
 
     //Calculates prices for all available offers that match this item id
-    return availableOffers
-        .where((offer) => itemId == offer.itemId)
+    return getOffersForItem(itemId: itemId)
         .map((e) => e.getPrice(itemCount: itemCount))
         //returns price with lowest final amount
         .reduce((price1, price2) =>
@@ -40,7 +52,7 @@ class CartModel {
   Price getTotalPrice() {
     Price initialPrice = Price(originalAmount: 0, finalAmount: 0);
     return items.entries
-        .map((e) => getPriceForItem(e.key))
+        .map((e) => getPriceForItem(itemId: e.key))
         .fold(initialPrice, (price1, price2) => price1 + price2);
   }
 
@@ -49,6 +61,4 @@ class CartModel {
       items[key] = 0;
     });
   }
-
-  CartModel({required this.currency, required this.availableOffers});
 }
