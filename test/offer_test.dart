be0efd2b884d8dd8f5c1994ad7a24a38702883da
Gil Nobrega <jgilfnuk@gmail.com>
2022-05-22 18:58:20 +0100

    Updates offer test with new price struct

--- test/offer_test.dart
+++ test/offer_test.dart
@@ -3,35 +3,46 @@ import 'package:shopping_app/Core/Offers/offer_multibuy_fixed.dart';
 import 'package:shopping_app/Core/Offers/offer_multibuy_n_for_n.dart';
 import 'package:shopping_app/Core/Offers/offer_none.dart';
 import 'package:shopping_app/Core/Offers/offer_single.dart';
-import 'package:shopping_app/Core/offer.dart';
+import 'package:shopping_app/Core/Offers/offer.dart';
+import 'package:shopping_app/Core/price.dart';
 
 void main() {
   test('Offer without discount', () {
     Offer offer = OfferNone(offerId: 1, originalUnitPrice: 100);
 
-    expect(offer.calculateDiscount(itemCount: 1), 0);
-    expect(offer.calculateDiscount(), 0);
-    expect(offer.calculateOriginalPrice(itemCount: 1), 100);
-    expect(offer.calculateFinalPrice(itemCount: 1), 100);
+    Price price1Item = offer.getPrice(itemCount: 1);
+    Price priceNoItems = offer.getPrice(itemCount: 0);
+
+    expect(price1Item.discountedAmount, 0);
+    expect(price1Item.originalAmount, 100);
+    expect(price1Item.finalAmount, 100);
+
+    expect(priceNoItems.discountedAmount, 0);
+    expect(priceNoItems.finalAmount, 0);
+    expect(priceNoItems.originalAmount, 0);
   });
   test('Offer with single discount', () {
     Offer offer =
         OfferSingle(offerId: 1, discountedAmount: 50, originalUnitPrice: 110);
 
-    //tests one unit
-    expect(offer.calculateOriginalPrice(itemCount: 1), 110);
-    expect(offer.calculateDiscount(itemCount: 1), 50);
-    expect(offer.calculateFinalPrice(itemCount: 1), 60);
-
-    //tests two units
-    expect(offer.calculateOriginalPrice(itemCount: 2), 220);
-    expect(offer.calculateDiscount(itemCount: 2), 100);
-    expect(offer.calculateFinalPrice(itemCount: 2), 120);
+    Price priceNoItems = offer.getPrice(itemCount: 0);
+    Price price1Item = offer.getPrice(itemCount: 1);
+    Price price2Items = offer.getPrice(itemCount: 2);
 
     //Tests no units
-    expect(offer.calculateOriginalPrice(), 0);
-    expect(offer.calculateDiscount(), 0);
-    expect(offer.calculateFinalPrice(), 0);
+    expect(priceNoItems.originalAmount, 0);
+    expect(priceNoItems.finalAmount, 0);
+    expect(priceNoItems.discountedAmount, 0);
+
+    //tests one unit
+    expect(price1Item.originalAmount, 110);
+    expect(price1Item.discountedAmount, 50);
+    expect(price1Item.finalAmount, 60);
+
+    //tests two units
+    expect(price2Items.originalAmount, 220);
+    expect(price2Items.discountedAmount, 100);
+    expect(price2Items.finalAmount, 120);
   });
 
   //Each face mask is 1£
@@ -40,25 +51,36 @@ void main() {
     Offer offer = OfferMultibuyFixed(
         offerId: 1, offerUnits: 3, offerAmount: 250, originalUnitPrice: 100);
 
-    //tests one unit
-    expect(offer.calculateOriginalPrice(itemCount: 1), 100);
-    expect(offer.calculateDiscount(itemCount: 1), 0);
-    expect(offer.calculateFinalPrice(itemCount: 1), 100);
-
-    //tests two units
-    expect(offer.calculateOriginalPrice(itemCount: 2), 200);
-    expect(offer.calculateDiscount(itemCount: 2), 0);
-    expect(offer.calculateFinalPrice(itemCount: 2), 200);
-
-    //tests three units
-    expect(offer.calculateOriginalPrice(itemCount: 3), 300);
-    expect(offer.calculateDiscount(itemCount: 3), 50);
-    expect(offer.calculateFinalPrice(itemCount: 3), 250);
+    Price priceNoItems = offer.getPrice(itemCount: 0);
+    Price price1Item = offer.getPrice(itemCount: 1);
+    Price price2Items = offer.getPrice(itemCount: 2);
+    Price price3Items = offer.getPrice(itemCount: 3);
+    Price price4Items = offer.getPrice(itemCount: 4);
 
     //Tests no units
-    expect(offer.calculateOriginalPrice(), 0);
-    expect(offer.calculateDiscount(), 0);
-    expect(offer.calculateFinalPrice(), 0);
+    expect(priceNoItems.originalAmount, 0);
+    expect(priceNoItems.finalAmount, 0);
+    expect(priceNoItems.discountedAmount, 0);
+
+    //tests one unit
+    expect(price1Item.originalAmount, 100);
+    expect(price1Item.discountedAmount, 0);
+    expect(price1Item.finalAmount, 100);
+
+    //tests two units
+    expect(price2Items.originalAmount, 200);
+    expect(price2Items.discountedAmount, 0);
+    expect(price2Items.finalAmount, 200);
+
+    //tests three units
+    expect(price3Items.originalAmount, 300);
+    expect(price3Items.discountedAmount, 50);
+    expect(price3Items.finalAmount, 250);
+
+    //tests four units
+    expect(price4Items.originalAmount, 400);
+    expect(price4Items.discountedAmount, 50);
+    expect(price4Items.finalAmount, 350);
   });
 
   //Each toilet paper roll is 65p
@@ -67,29 +89,41 @@ void main() {
     Offer offer = OfferMultibuyNForN(
         offerId: 1, offerUnits: 4, forUnits: 3, originalUnitPrice: 65);
 
-    //tests one unit
-    expect(offer.calculateOriginalPrice(itemCount: 1), 65);
-    expect(offer.calculateDiscount(itemCount: 1), 0);
-    expect(offer.calculateFinalPrice(itemCount: 1), 65);
-
-    //tests two units
-    expect(offer.calculateOriginalPrice(itemCount: 2), 130);
-    expect(offer.calculateDiscount(itemCount: 2), 0);
-    expect(offer.calculateFinalPrice(itemCount: 2), 130);
-
-    //tests three units
-    expect(offer.calculateOriginalPrice(itemCount: 3), 195);
-    expect(offer.calculateDiscount(itemCount: 3), 0);
-    expect(offer.calculateFinalPrice(itemCount: 3), 195);
-
-    //tests four units
-    expect(offer.calculateOriginalPrice(itemCount: 4), 260);
-    expect(offer.calculateDiscount(itemCount: 4), 65);
-    expect(offer.calculateFinalPrice(itemCount: 4), 195);
+    Price priceNoItems = offer.getPrice(itemCount: 0);
+    Price price1Item = offer.getPrice(itemCount: 1);
+    Price price2Items = offer.getPrice(itemCount: 2);
+    Price price3Items = offer.getPrice(itemCount: 3);
+    Price price4Items = offer.getPrice(itemCount: 4);
+    Price price5Items = offer.getPrice(itemCount: 5);
 
     //Tests no units
-    expect(offer.calculateOriginalPrice(), 0);
-    expect(offer.calculateDiscount(), 0);
-    expect(offer.calculateFinalPrice(), 0);
+    expect(priceNoItems.originalAmount, 0);
+    expect(priceNoItems.finalAmount, 0);
+    expect(priceNoItems.discountedAmount, 0);
+
+    //tests one unit
+    expect(price1Item.originalAmount, 65);
+    expect(price1Item.discountedAmount, 0);
+    expect(price1Item.finalAmount, 65);
+
+    //tests two units
+    expect(price2Items.originalAmount, 130);
+    expect(price2Items.discountedAmount, 0);
+    expect(price2Items.finalAmount, 130);
+
+    //tests three units
+    expect(price3Items.originalAmount, 195);
+    expect(price3Items.discountedAmount, 0);
+    expect(price3Items.finalAmount, 195);
+
+    //tests four units
+    expect(price4Items.originalAmount, 260);
+    expect(price4Items.discountedAmount, 65);
+    expect(price4Items.finalAmount, 195);
+
+    //tests five units
+    expect(price5Items.originalAmount, 325);
+    expect(price5Items.discountedAmount, 65);
+    expect(price5Items.finalAmount, 260);
   });
 }
